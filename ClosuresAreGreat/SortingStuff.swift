8b47e978c80a857e2be960a02dcd8c26b01e8a8e
EnriqueTor <etorrendell@gmail.com>
2016-10-26 14:22:17 -0400

    Complete, no idea if it is good or a disaster

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -16,6 +16,74 @@ struct ToyBin {
     var bowlingPins: [BowlingPin] = []
     var musicCDs: [MusicCD] = []
     
+    func sortShps() {
+        
+        ships.sorted { (ship1:Ship, ship2:Ship) -> Bool in
+            
+            return ship1.age > ship2.age
+        }
+    }
+    
+    func sortBooks() {
+        
+        books.sorted {
+            
+            return ($0).name < ($1).name
+        }
+    }
+    
+    func sortBowlingPins() {
+        
+        bowlingPins.sorted {
+            
+            return ($0).color.rawValue < ($1).color.rawValue
+        }
+    }
+    
+    func sortMusicCDs() {
+        
+        musicCDs.sorted { (cd1: MusicCD, cd2: MusicCD) -> Bool in
+            
+            if cd1.name < cd2.name {
+                
+                return true
+            }
+            
+          if cd1.name == "Drake" {
+            
+            musicCDs.dropFirst()
+            
+            return true
+         }
+        
+            return false
+        }
+    }
+    
+    func changeColorOfAllPins(to color: Color) {
+        
+        bowlingPins.map { (bow1: BowlingPin) -> [BowlingPin] in
+            
+            var newArray: [BowlingPin] = []
+            newArray.append(BowlingPin(name: bow1.name, film: bow1.film, color: color))
+
+            return newArray
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     // TODO: Implement all of the sort functions (lets organize this toy bin!)
     
  
